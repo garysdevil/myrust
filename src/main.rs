5811ce4b6fb224dd764355978e52419bc729e3d6
sherry <adamgarysdevil@gmail.com>
2022-06-21 18:41:57 +0800

    20220621

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod local1;
-mod local2;
+mod local_1;
+mod local_2;
 mod pratice;
 mod root;
 mod test;
@@ -11,5 +11,5 @@ fn main() {
     // pratice::pratice_2::main(); // cargo run --features feature1
     // pratice::pratice_5::main(); // cargo run --features feature2
 
-    local1::local_fn(); // 运行 local1.rs 文件里的local_fn方法，此文件只存在于本地，不上传进代码仓库。
+    local_1::local_fn(); // 运行 local1.rs 文件里的local_fn方法，此文件只存在于本地，不上传进代码仓库。
 }
--- src/pratice/clap.rs
+++ src/pratice/clap.rs
@@ -1,14 +1,22 @@
+// 命令行输入参数
+
+#![allow(dead_code)]
+
 use clap::Parser;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Cli {
     #[clap(long)]
-    two: String,
+    one: String, // String类型的参数必须输入，否则会报错
     #[clap(long)]
-    one: String,
+    two: bool, // bool类型的参数默认值为false
+    #[clap(default_value = "33", long)] // default_value = "33" 定义默认值为“33”
+    three: String,
+    #[clap(hide = true, long)] // hide = true 定义在--help时不显示帮助
+    four: String,
     #[clap(long)]
-    pub three: Option<String>, // 使用Option<>，则这个变量可以为None，在输入参数时可以不进行输入
+    pub five: Option<String>, // 使用Option<>，则这个变量可以为None，在输入参数时可以不进行输入，否则启动时不输入这个参数会报错
 }
 
 pub fn main() {
