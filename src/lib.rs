ffe6fe2863c88c120c378d9bc52481c076fdcb78
sherry <adamgarysdevil@gmail.com>
2022-06-28 23:39:23 +0800

    update test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 pub fn main() {
     println!("I am the main() function in lib.rs");
 }
+
+// 暴露模块给宝箱外部使用 // 只能从库宝箱的源文件向包的外部暴露模块
+pub mod pratice;
---
+++ tests/my_test.rs
@@ -0,0 +1,7 @@
+use myrust::pratice;
+
+#[test]
+fn my_test1() {
+    pratice::pratice_1::main();
+    assert_eq!(4, 4);
+}
