083994a4cb4d20cf0d7c549aabd9c09cb4ab5d57
sherry <adamgarysdevil@gmail.com>
2022-08-23 15:40:16 +0800

    add cuda example

---
+++ mycuda/README.md
@@ -0,0 +1,5 @@
+## 使用rustacuda进行GPU编程
+
+```bash
+cargo run --example example_1
+```
---
+++ mycuda/src/main.rs
@@ -0,0 +1,84 @@
+#[macro_use]
+extern crate rustacuda;
+
+use rustacuda::prelude::*;
+use std::error::Error;
+use std::ffi::CString;
+
+fn main() -> Result<(), Box<dyn Error>> {
+        // Set up the context, load the module, and create a stream to run kernels in.
+    // 设置上下文，加载模块，创建和内核交互的数据流
+    rustacuda::init(CudaFlags::empty())?;
+    let device = Device::get_device(1)?;
+    dbg!(&device);
+    let _ctx = Context::create_and_push(ContextFlags::MAP_HOST | ContextFlags::SCHED_AUTO, device)?;
+
+    let ptx = CString::new(include_str!("../resources/add.ptx"))?;
+
+    for i in 0..10000{
+        match test(ptx.clone()){
+            Ok(()) => println!("{}. Great~", i),
+            Err(error) => println!("{}", error)
+        };
+    }
+    
+    Ok(())
+}
+fn test(ptx: CString) -> Result<(), Box<dyn Error>> {
+    let module = Module::load_from_string(&ptx)?;
+    let stream = Stream::new(StreamFlags::NON_BLOCKING, None)?;
+
+    // Create buffers for data
+    // 为数据创建buffers
+    let mut in_x = DeviceBuffer::from_slice(&[1.0f32; 1000000])?;
+    let mut in_y = DeviceBuffer::from_slice(&[2.0f32; 1000000])?;
+    let mut out_1 = DeviceBuffer::from_slice(&[0.0f32; 1000000])?;
+    let mut out_2 = DeviceBuffer::from_slice(&[0.0f32; 1000000])?;
+
+    // This kernel adds each element in `in_x` and `in_y` and writes the result into `out`.
+    // 
+    unsafe {
+        // Launch the kernel with one block of one thread, no dynamic shared memory on `stream`.
+        // 方式一 通过一个线程一个块启动GPU内核，在 stream 上没有动态共享内存。
+        let result = launch!(module.sum<<<1, 1, 0, stream>>>(
+            in_x.as_device_ptr(),
+            in_y.as_device_ptr(),
+            out_1.as_device_ptr(),
+            out_1.len()
+        ));
+        result?;
+
+
+        // Launch the kernel again using the `function` form:
+        // 方式二 通过函数启动GPU内核
+        let function_name = CString::new("sum")?;
+        let sum = module.get_function(&function_name)?;
+        // Launch with 1x1x1 (1) blocks of 10x1x1 (10) threads, to show that you can use tuples to
+        // configure grid and block size.
+        let result = launch!(sum<<<(1, 1, 1), (10, 1, 1), 0, stream>>>(
+            in_x.as_device_ptr(),
+            in_y.as_device_ptr(),
+            out_2.as_device_ptr(),
+            out_2.len()
+        ));
+        result?;
+    }
+
+    // Kernel launches are asynchronous, so we wait for the kernels to finish executing.
+    // 等待GPU内核执行结束
+    stream.synchronize()?;
+
+    // Copy the results back to host memory
+    // 将GPU计算结果拷贝回主机内存
+    let mut out_host = [0.0f32; 2000000];
+    out_1.copy_to(&mut out_host[0..1000000])?;
+    out_2.copy_to(&mut out_host[1000000..2000000])?;
+
+    // 判断计算结果是否符合预期
+    for x in out_host.iter() {
+        // println!("{}", *x);
+        assert_eq!(3.0 as u32, *x as u32);
+    }
+
+    Ok(())
+}
